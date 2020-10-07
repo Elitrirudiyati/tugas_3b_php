eac48df2c28d2a36194e363b728958cef8e5ab4d
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-10-07 17:00:23 +0700

    Add files via upload

---
+++ tugas_3b.php
@@ -0,0 +1,7 @@
+<?php
+$tab_awal=150000;
+$bunga_tahun=12.5;
+$tab_akhir=$tab_awal+($tab_awal*$bunga_tahun/100);
+
+echo "Tabungan Awal = Rp $tab_awal <br> Bunga Setahun = $bunga_tahun% <br> Total Tabungan Setahun =? <br> Jawaban : Rp $tab_akhir";
+?> 
